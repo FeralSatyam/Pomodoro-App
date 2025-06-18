f2f4351393cc89c4cf253600d2f0e226503d72ed
FeralSatyam <satyamranaofficiall@gmail.com>
2025-06-18 15:21:16 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import DigitalClock from "./DigitalClock"
+import StopWatch from "./StopWatch"
+function App() {
+  return (
+    <>
+    <title>Pomodoro</title>
+      <DigitalClock></DigitalClock>
+      <StopWatch></StopWatch>
+    </>
+  )
+}
+
+export default App
---
+++ src/StopWatch.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect, useRef} from 'react'
+function StopWatch(){
+
+
+    return(
+        <></>
+    );
+}   
+export default StopWatch
